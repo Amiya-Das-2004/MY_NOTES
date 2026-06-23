e240edb3b361cc0290456dfd28ee3778eeb35b4e
Amiya_Das <22ce01057@iitbbs.aac.in>
2026-06-24 01:27:50 +0530

    something is wrong

--- JS/Sidebar_Menu.js
+++ JS/Sidebar_Menu.js
@@ -37,25 +37,38 @@
             title: 'Introduction',
             href: 'MY_NOTES/Index.html',
             active: currentUrl.endsWith('Index.html') || currentUrl === '/' || currentUrl.match(/MY_NOTES\/?(Index\.html)?$/)
+        },
+        {
+            type: 'folder',
+            label: 'Topology Optimization',
+            id: 'Topology_Optimization',
+            open: currentUrl.includes('/Topology_Optimization/'),
+            pages: [
+                {
+                    title: 'Phase Field',
+                    url: 'MY_NOTES/NOTES/Topology_Optimization/Phase_Field.html',
+                    active: currentUrl.endsWith('Phase_Field.html')
+                },
+                {
+                    title: 'Reaction Diffusion',
+                    url: 'MY_NOTES/NOTES/Topology_Optimization/Reaction_Diffusion.html',
+                    active: currentUrl.endsWith('Reaction_Diffusion.html')
+                }
+            ]
+        },
+        {
+            type: 'folder',
+            label: 'Continuum Mechanics',
+            id: 'Continnum_Mechanics',
+            open: currentUrl.includes('/Continuum_Mechanics/'),
+            pages: [
+                {
+                    title: 'Vectors',
+                    url: 'MY_NOTES/NOTES/Continuum_Mechanics/Vectors.html',
+                    active: currentUrl.endsWith('Vectors.html')
+                }
+            ]
         }
-        // {
-        //     type: 'folder',
-        //     label: 'Topology Optimization',
-        //     id: 'Topology_Optimization',
-        //     open: currentUrl.includes('/Topology_Optimization/'),
-        //     pages: [
-        //         {
-        //             title: 'Phase Field',
-        //             url: 'MY_NOTES/NOTES/Topology_Optimization/Phase_Field.html',
-        //             active: currentUrl.endsWith('Phase_Field.html')
-        //         },
-        //         {
-        //             title: 'Reaction Diffusion',
-        //             url: 'MY_NOTES/NOTES/Topology_Optimization/Reaction_Diffusion.html',
-        //             active: currentUrl.endsWith('Reaction_Diffusion.html')
-        //         }
-        //     ]
-        // }
     ];
 
     // Function: escHtml(s)
